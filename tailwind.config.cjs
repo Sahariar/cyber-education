2c56f433d0fc96f61f0b68838a0c91bf8bc60f01
Sahariar Kabir <sronok@gmail.com>
2022-10-24 23:38:01 +0600

    added theme colors and logo site design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,50 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	daisyui: {
+		themes: [
+			{
+				cyberlight: {
+					primary: "#FFCD34",
+
+					secondary: "#113385",
+
+					accent: "#F75F05",
+
+					neutral: "#352A3C",
+
+					"base-100": "#FBFCFD",
+
+					info: "#0ea5e9",
+
+					success: "#16a34a",
+
+					warning: "#a16207",
+
+					error: "#f43f5e",
+				},
+				cyberdark: {
+					primary: "#ce5654",
+
+					secondary: "#3f98d3",
+
+					accent: "#c729d8",
+
+					neutral: "#1A191F",
+
+					"base-100": "#2A2447",
+
+					info: "#3656E2",
+
+					success: "#1A9E4C",
+
+					warning: "#F0C54C",
+
+					error: "#F54D85",
+				},
+			},
+		],
+    darkTheme: "cyberdark",
+	},
+	plugins: [require("daisyui")],
+};
