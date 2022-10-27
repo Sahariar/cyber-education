b5c16689630afeafe0b4745fbbad5b67aeb8fa83
Sahariar Kabir <sronok@gmail.com>
2022-10-27 14:20:13 +0600

    adding sort filter option

--- src/context/DataProvider.jsx
+++ src/context/DataProvider.jsx
@@ -2,35 +2,121 @@ import React, { createContext, useEffect, useState } from "react";
 
 export const DataContext = createContext();
 
-const DataProvider = ({children}) => {
-    const [dataCate, setDataCate ] = useState([]);
-    const [feature, setFeature ] = useState([]);
-    useEffect( ()=>{
-    const getCateData = async () => {
-        const response = await fetch("https://cyber-education-server.vercel.app/cate");
-        const data = await response.json();
-        setDataCate(data);
-    }
-    getCateData()
-  
-    },[])
-    useEffect( ()=>{
-    const getCateData = async () => {
-        const response = await fetch("https://cyber-education-server.vercel.app/feature");
-        const data = await response.json();
-        setFeature(data);
-    }
-    getCateData()
-  
-    },[])
+const DataProvider = ({ children }) => {
+	const [dataCate, setDataCate] = useState([]);
+	const [feature, setFeature] = useState([]);
+	const [hot, setHot] = useState([]);
+	const [special, setSpecial] = useState([]);
+	const [sortPrice, setSortPrice] = useState([]);
+	const [sortPriceDes, setPriceDes] = useState([]);
+	const [sortRating, setRating] = useState([]);
+	const [sortRatingDes, setRatingDes] = useState([]);
+	useEffect(() => {
+		const getCateData = async () => {
+			const response = await fetch(
+				"https://cyber-education-server.vercel.app/cate"
+			);
+			const data = await response.json();
+			setDataCate(data);
+		};
+		getCateData();
+	}, []);
+	useEffect(() => {
+		const getCateData = async () => {
+			const response = await fetch(
+				"https://cyber-education-server.vercel.app/feature"
+			);
+			const data = await response.json();
+			setFeature(data);
+		};
+		getCateData();
+	}, []);
+	useEffect(() => {
+		const getCateData = async () => {
+			const response = await fetch(
+				"https://cyber-education-server.vercel.app/hot"
+			);
+			const data = await response.json();
+			setHot(data);
+		};
+		getCateData();
+	}, []);
+	useEffect(() => {
+		const getCateData = async () => {
+			const response = await fetch(
+				"https://cyber-education-server.vercel.app/special"
+			);
+			const data = await response.json();
+			setSpecial(data);
+		};
+		getCateData();
+	}, []);
+	useEffect(() => {
+		const getCateData = async () => {
+			const response = await fetch(
+				"https://cyber-education-server.vercel.app/new"
+			);
+			const data = await response.json();
+			setSortNew(data);
+		};
+		getCateData();
+	}, []);
+	useEffect(() => {
+		const getCateData = async () => {
+			const response = await fetch(
+				"https://cyber-education-server.vercel.app/price"
+			);
+			const data = await response.json();
+			setSortPrice(data);
+		};
+		getCateData();
+	}, []);
+	useEffect(() => {
+		const getCateData = async () => {
+			const response = await fetch(
+				"https://cyber-education-server.vercel.app/rating"
+			);
+			const data = await response.json();
+			setRating(data);
+		};
+		getCateData();
+	}, []);
+	useEffect(() => {
+		const getCateData = async () => {
+			const response = await fetch(
+				"https://cyber-education-server.vercel.app/rating/desc"
+			);
+			const data = await response.json();
+			setRatingDes(data);
+		};
+		getCateData();
+	}, []);
+	useEffect(() => {
+		const getCateData = async () => {
+			const response = await fetch(
+				"https://cyber-education-server.vercel.app/price/desc"
+			);
+			const data = await response.json();
+			setPriceDes(data);
+		};
+		getCateData();
+	}, []);
 
-    const dataValue = { dataCate , feature }
+	const dataValue = {
+		dataCate,
+		feature,
+		hot,
+		special,
+		sortNew,
+		sortPrice,
+		sortPriceDes,
+		sortRating,
+		sortRatingDes,
+	};
 
-    return (
-        <DataContext.Provider value={dataValue}>
-                { children }
-        </DataContext.Provider>
-    );
+	return (
+		<DataContext.Provider value={dataValue}>{children}</DataContext.Provider>
+	);
 };
 
 export default DataProvider;
